44c4a7cdbfe9088d59f24ea932052b3a5ccaa658
lyc-mac <liyongchangwyy@163.com>
2022-07-28 23:48:46 +0800

    菜单

--- code/通讯录管理系统/main.cpp
+++ code/通讯录管理系统/main.cpp
@@ -1,3 +1,32 @@
 //
-// Created by 李永昌 on 2022/7/27.
+// Created by ycl on 2022/7/27.
 //
+
+#include <iostream>
+using namespace std;
+
+// 函数原型
+void showMenu(void);
+
+// 主程序
+int main()
+{
+    // 显示菜单
+    showMenu();
+
+    return 0;
+}
+
+
+// 菜单界面
+void showMenu()
+{
+   cout << "1、添加联系人" << endl;
+   cout << "2、显示联系人" << endl;
+   cout << "3、删除联系人" << endl;
+   cout << "4、查找联系人" << endl;
+   cout << "5、修改联系人" << endl;
+   cout << "6、清空联系人" << endl;
+   cout << "0、添加联系人" << endl;
+
+}
