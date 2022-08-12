6923aa12fef0605eceae1c6f041a6768c3aca8de
lyc-mac <liyongchangwyy@163.com>
2022-08-12 22:48:23 +0800

    退出功能

--- code/通讯录管理系统/main.cpp
+++ code/通讯录管理系统/main.cpp
@@ -7,14 +7,54 @@
 using namespace std;
 
 // 函数原型
+// 显示菜单
 void showMenu(void);
 
 // 主程序
 int main()
 {
-    // 显示菜单
-    showMenu();
+    int select = 0; //创建用户选择输入的变量
 
+    while (true)
+    {
+         // 显示菜单
+        showMenu();
+
+        // 获取用户输入
+        cin >> select;
+
+        // 根据用户输入判断实现功能
+        switch (select)
+        {
+        case 1: // 添加联系人
+            
+            break;
+        case 2: // 显示联系人
+            
+            break;
+        case 3: // 删除联系人
+            
+            break;
+        case 4: // 查找联系人
+            
+            break;
+        case 5: // 修改联系人
+            
+            break;
+        case 6: // 清空联系人
+            
+            break;
+        case 0: //
+            cout << "感谢使用！" << endl;
+            return 0;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+   
     return 0;
 }
 
@@ -28,6 +68,9 @@ void showMenu()
    cout << "4、查找联系人" << endl;
    cout << "5、修改联系人" << endl;
    cout << "6、清空联系人" << endl;
-   cout << "0、添加联系人" << endl;
-
+   cout << "0、退出通讯录" << endl;
+   cout << "请选择功能..." << endl;
 }
+
+// 退出
+// void 
