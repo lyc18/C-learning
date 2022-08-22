592b878027f56d7d8db60c178f10bc301de3ee07
lyc-mac <liyongchangwyy@163.com>
2022-08-22 14:27:34 +0800

    结构体

--- code/通讯录管理系统/main.cpp
+++ code/通讯录管理系统/main.cpp
@@ -4,11 +4,33 @@
 
 
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+//常量定义
+#define MAX 1000 // 最大人数
 
 // 函数原型
 // 显示菜单
 void showMenu(void);
+
+// 结构体
+// 联系人结构体
+struct Person
+{
+    string m_Name; //姓名
+    int m_Sex; // 性别：1:男，2:女
+    int m_Age; // 年龄
+    string m_Phone; // 电话
+    string m_Addr; // 住址
+};
+
+// 通讯录结构体
+struct Addressbooks
+{
+    struct Person personArray[MAX]; // 通讯录中保存的联系人数组
+    int m_Size; // 通讯录中人员个数
+};
 
 // 主程序
 int main()
@@ -72,5 +94,8 @@ void showMenu()
    cout << "请选择功能..." << endl;
 }
 
-// 退出
-// void 
+// 添加联系人
+void addContact()
+{
+    
+}
